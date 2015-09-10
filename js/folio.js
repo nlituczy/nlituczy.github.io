ebeef2886908654f3b221b374ad8509766c9071e
nlituczy <nlituczy@gmail.com>
2015-09-10 15:40:06 -0400

    Complete Overhaul
    This is a complete overhaul of the site. It is a step closer to the
    real thing.

--- js/folio.js
+++ js/folio.js
@@ -7,402 +7,189 @@
  *
  * Copyright 2015, Nicholas Lituczy
  * http://www.nicholaslituczy.com
-
-              /\‚
-            /    \ ‚
-          /        \‘
-         |\          \
-       /\|::\         '\  '
-     /    \:::\         \‚
-   /        \:::\        '\ °
-  |\          \::|         |'
-  |:|       |\  \/        /|‘
-  |/       /|::\________/::|'
- /       /::|:::|::::::|:::|‘
-|\     /::::|\::|::::::|::/‘
-|::\ /:::::/   \|::::::|/  '
-|:::|::::/       ¯¯¯¯¯¯¯
- \::|::/
-   \|/
-
-
 **/
 
 $(document).ready(function(){
 
-//dropdown menu
-  /*$('.menu-icon').click(function(){
-  	$(this).toggleClass('open');
-    $('#menu-select > li').toggleClass('open');
-  });*/
+/* -----------------------
+    Page Navigation
+-------------------------*/
 
-//resume replace view
-  $('#about').click(function(){
-	   $('body').toggleClass('page-open');
-     $('#contact-form').toggleClass('open');
+// Scroll to location
+
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate( {
+            'scrollTop': $target.offset().top-40
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        } );
+    } );
+
+  $(function() {
+
+      var $el, leftPos, newWidth,
+          $mainNav = $("#menu-links");
+
+      $mainNav.append("<li id='#magic-line'></li>");
+      var $magicLine = $("#magic-line");
+
+      $magicLine
+          .width($(".current-page-item").width())
+          .css("left", $(".current-page-item a").position().left)
+          .data("origLeft", $magicLine.position().left)
+          .data("origWidth", $magicLine.width());
+
+      $("#menu-links li a").hover(function() {
+          $el = $(this);
+          leftPos = $el.position().left;
+          newWidth = $el.parent().width();
+          $magicLine.stop().animate({
+              left: leftPos,
+              width: newWidth
+          });
+      }, function() {
+          $magicLine.stop().animate({
+              left: $magicLine.data("origLeft"),
+              width: $magicLine.data("origWidth")
+          });
+      });
   });
-
-//contact replace view
-  $('#contact').click(function(){
-	   $('body').toggleClass('page-open');
-     $('#contact-form').toggleClass('open');
-  });
-
-//contact replace view
-  $('#twitter').click(function(){
-	   $('body').toggleClass('page-open');
-     $('#contact-form').toggleClass('open');
-  });
-// Write some text
-
 
 /* -----------------------
-    Text Data
+    Background Loop
 -------------------------*/
-  var sections = [
-  		{	sentence: " am Nicholas Lituczy",
-  		},
-  		{	sentence: " am a product designer",
-  		},
-  		{	sentence: " live in New York city",
-  		},
-  		{	sentence: " am a builder",
-  		},
-  		{	sentence: " create",
-  		},
-  		{	sentence: " explore",
-  		},
-  		{	sentence: " learn",
-  		},
-  		{	sentence: " dream",
-  		},
-  		{	sentence: " build",
-  		},
-  		{	sentence: " am doing another usability test",
-  		},
-  		{	sentence: " am making my nth cup of coffee",
-  		},
-  		{	sentence: " am debugging this website",
-  		},
-  		{	sentence: " am reading about data driven design",
-  		},
-  		{	sentence: " obsessed with No Man's Sky",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  		{	sentence: " ",
-  		},
-  	];
-  var i = 0;
-  var j = 0;
-  var k = 0;
-  var lengthSentence = 0;
-  var lengthArray = sections.length;
-  var forward = true;
-  var beginning = "I";
-  var currentPart = "";
-  var interval = 50;
-  var opening = false;
 
-  /* -----------------------
-      TYPING
-  -------------------------*/
-  function writing(text){
-  	lengthSentence = sections[i].sentence.length;
-  	var body = $("body");
-  	if(!opening){ // first part
-  		setTimeout(function(){
-  			if(k < beginning.length){
-  				if(beginning[k] === "<"){
+  var canvas = document.querySelector("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var ctx = canvas.getContext("2d");
 
-  					k=k+4;
-  				}
-  				currentPart += beginning[k];
-  				text.html(currentPart);
-  				k++;
-  				writing(text);
-  			}else if(k === (beginning.length)){
-  				text.html(currentPart);
-  				opening = true;
-  				writing(text);
-  			}
-  		},interval);
-  	}else if(opening){ // sentences
-  		setTimeout(function(){
-  			interval = 50;
-  			if(j === lengthSentence){
-  				forward = false;
-  			}
-  			if(j === lengthSentence-2){
-  				$(".afterTyping").one().attr("onScreen");
-  			}
-  			if( j === lengthSentence-1 && forward){
-  				interval = 2000;
-  			}
-  			if(j < lengthSentence && forward ){
-  				if(sections[i].sentence[j] === "&"){
-  					currentPart += "<strong>";
-  				}else if(sections[i].sentence[j] === "%"){
-  					currentPart += "</strong>";
-  				}
-  				else{
-  					currentPart += sections[i].sentence[j];
-  				}
-  				text.html(currentPart);
-  				j++;
-  			}else if(j > 0 && !forward){
-  				if(sections[i].sentence[j] === "&"){
-  					currentPart = currentPart.slice(0, - 8);
-  				}else if(sections[i].sentence[j] === "%"){
-  					currentPart = currentPart.slice(0, - 9);
-  				}
-  				else{
-  					currentPart = currentPart.slice(0, - 1);
-  				}
-  				text.html(currentPart);
-  				j--;
-  			}else if(j === 0){
-  				forward = true;
-  				/*body.css({
-  					"background" : sections[i].background});*/
-  				i++; // loop fra sezioni
-  			}
-  			if(i === lengthArray){
-  				i = 0;
-  			}
-  			writing(text);
-  		}, interval);
-  	}
+  var TAU = 2 * Math.PI;
+
+  times = [];
+  function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    update();
+    draw();
+    requestAnimationFrame(loop);
   }
 
-
-  /* -----------------------
-      BACKGROUND LOOP
-  -------------------------*/
-
-
-var colors = new Array(
-  [62,35,255],
-  [60,255,60],
-  [255,35,98],
-  [45,175,230],
-  [255,0,255],
-  [255,128,0]);
-
-var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0,1,2,3];
-
-//transition speed
-var gradientSpeed = 0.002;
-
-function updateGradient()
-{
-
-  if ( $===undefined ) return;
-
-var c0_0 = colors[colorIndices[0]];
-var c0_1 = colors[colorIndices[1]];
-var c1_0 = colors[colorIndices[2]];
-var c1_1 = colors[colorIndices[3]];
-
-var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "rgb("+r1+","+g1+","+b1+")";
-
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "rgb("+r2+","+g2+","+b2+")";
-
- $('#container').css({
-   background: "-webkit-gradient(linear, left bottom, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 50%, "+color2+" 50%)"});
-
-  step += gradientSpeed;
-  if ( step >= 1 )
-  {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-
-  }
-}
-
-setInterval(updateGradient,50);
-
-/*
-
-  function rand(min, max) {
-      return min + Math.random() * (max - min);
-  }
-  function changebackground(){
-  	var body = $("body");
-      var h = rand(1, 360);
-      var s = rand(80, 90);
-      var l = rand(50, 60);
-      var h2;
-      if(h < 180){
-      	h2 = h + 180;
-      }else{
-      	h2 = h - 180;
+  function Ball (startX, startY, startVelX, startVelY) {
+    this.x = startX || Math.random() * canvas.width;
+    this.y = startY || Math.random() * canvas.height;
+    this.vel = {
+      x: startVelX || Math.random() * 2 - 1,
+      y: startVelY || Math.random() * 2 - 1
+    };
+    this.update = function(canvas) {
+      if (this.x > canvas.width + 50 || this.x < -50) {
+        this.vel.x = -this.vel.x;
       }
-      body.css({ // looping background
-      	"background" : "hsl(" + h + "," + s + "%,"+ l + "%)"
-      });
-      $(".fixedBg").css({ // background on hover
-      	"background" : "hsl(" + h + "," + s + "%,"+ l + "%)",
-      	"color" : "hsl(" + h2 + "," + s + "%,"+ l + "%)"
-      });
-      /*$(".loopCol").css({
-      	"background" : "hsl(" + h + "," + s + "%,"+ l + "%)"
-      });
-  	$(".coloredHover").css({ // color links on hover
-      	"color" : "hsl(" + h + "," + s + "%,"+ l + "%)"
-      });
-
+      if (this.y > canvas.height + 50 || this.y < -50) {
+        this.vel.y = -this.vel.y;
+      }
+      this.x += this.vel.x;
+      this.y += this.vel.y;
+    };
+    this.draw = function(ctx, can) {
+      ctx.beginPath();
+      ctx.globalAlpha = .4;
+      ctx.fillStyle = '#EF565A';
+      ctx.arc((0.5 + this.x) | 0, (0.5 + this.y) | 0, 3, 0, TAU, false);
+      ctx.fill();
+    }
   }
 
-
-
-// background loop - old links colors
-  function loopColors(){
-  	var selector = $(".loopCol");
-      var h = rand(1, 360);
-      var s = rand(0, 100);
-      var l = rand(0, 80);
-      selector.css({
-      	"color" : "hsl(" + h + "," + s + "%,"+ l + "%)"
-      });
+  var balls = [];
+  for (var i = 0; i < canvas.width * canvas.height / (65*65); i++) {
+    balls.push(new Ball(Math.random() * canvas.width, Math.random() * canvas.height));
   }
 
-//background loop
-	changebackground();
-	setTimeout(function(){
-		$("body").removeAttr("noTransition");
-		$("fixedBg").removeAttr("noTransition");
-		changebackground();
-	}, 2000)
-	setInterval(function(){
-		changebackground();
-	}, 20000);
+  var lastTime = Date.now();
+  function update() {
+    var diff = Date.now() - lastTime;
+    for (var frame = 0; frame * 99< diff; frame++) {
+      for (var index = 0; index < balls.length; index++) {
+        balls[index].update(canvas);
+      }
+    }
+    lastTime = Date.now();
+  }
 
-  */
+  function draw() {
+    ctx.globalAlpha=1;
+    ctx.fillStyle = '#F4F1EF';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    for (var index = 0; index < balls.length; index++) {
+      var ball = balls[index];
+      ball.draw(ctx, canvas);
+      ctx.beginPath();
+      for (var index2 = balls.length -1; index2 > index; index2 += -1) {
+        var ball2 = balls[index2];
+        var dist = Math.hypot(ball.x - ball2.x, ball.y - ball2.y);
+          if (dist < 100) {
+            ctx.strokeStyle = "#EF565A";
+            ctx.globalAlpha = 1 - (dist > 100 ? .8 : dist / 150);
+            ctx.lineWidth = "2px";
+            ctx.moveTo((0.5 + ball.x) | 0, (0.5 + ball.y) | 0);
+            ctx.lineTo((0.5 + ball2.x) | 0, (0.5 + ball2.y) | 0);
+          }
+      }
+      ctx.stroke();
+    }
+  }
+
+  // Start
+  loop();
+
 
 /*--------------------
-	Type stuff
+	 View Stack
 ----------------------*/
-	var firstTimer = 5000;
-	var text = $(".jstext");
-	setTimeout(function(){
-		writing(text);
-		//incipit(text);
-	}, firstTimer);
-
-/*--------------------
-	view fix
-----------------------*/
-/*
   (function(window){
   $.fn.stopAtTop= function () {
-    var $this = this,
-        $window = $(window),
-        thisPos = $this.offset().top,
-        //thisPreservedTop = $this.css("top"),
-        setPosition,
-        under,
-        over;
+      var $this = this,
+          $window = $(window),
+          thisPos = $this.offset().top,
+          //thisPreservedTop = $this.css("top"),
+          setPosition,
+          under,
+          over;
 
-    under = function(){
-        if ($window.scrollTop() < thisPos) {
-            $this.css({
-                position: 'absolute',
-                top: ""
-            });
-            setPosition = over;
-        }
-    };
+      under = function(){
+          if ($window.scrollTop() < thisPos) {
+              $this.css({
+                  position: 'absolute',
+                  top: ""
+              });
+              setPosition = over;
+          }
+      };
 
-    over = function(){
-        if (!($window.scrollTop() < thisPos)){
-            $this.css({
-                position: 'fixed',
-                top: 0
-            });
-            setPosition = under;
-        }
-    };
+      over = function(){
+          if (!($window.scrollTop() < thisPos)){
+              $this.css({
+                  position: 'fixed',
+                  top: 0
+              });
+              setPosition = under;
+          }
+      };
 
-    setPosition = over;
+      setPosition = over;
 
-    $window.resize(function()
-    {
-        bumperPos = pos.offset().top;
-        thisHeight = $this.outerHeight();
-        setPosition();
-    });
+      $window.resize(function()
+      {
+          thisHeight = $this.outerHeight();
+          setPosition();
+      });
 
-    $window.scroll(function(){setPosition();});
-    setPosition();
+      $window.scroll(function(){setPosition();});
+      setPosition();
   };
   })(window);
 
@@ -410,5 +197,5 @@ setInterval(updateGradient,50);
   $('#two').stopAtTop();
   $('#three').stopAtTop();
   $('#four').stopAtTop();
-*/
+
 });
