c7af27473ede1611610e0bffcbcf37ae405be60f
nlituczy <nlituczy@gmail.com>
2015-09-17 14:11:29 -0400

    broke stuff
    changed the footer
    started adding media to the preview card

--- js/folio.js
+++ js/folio.js
@@ -198,4 +198,26 @@ $(document).ready(function(){
   $('#three').stopAtTop();
   $('#four').stopAtTop();
 
+
+
+/*--------------------
+	 Social Media
+----------------------*/
+  $('.popup').click(function(event) {
+    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+
+    window.open(url, 'twitter', 'linkedin', opts);
+
+    return false;
+  });
+
 });
